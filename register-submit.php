c3faa80e2a59c76b2e5bfd1c7c61ab38cddc8928
ankur@ankur-laptop <none@none>
2010-01-27 16:17:27 -0800

    Fixed bug with empty db values

--- register-submit.php
+++ register-submit.php
@@ -4,7 +4,6 @@
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$phone = preg_replace('/[^0-9]/', '', $_POST['phone']);
-	$success = addPerson($name, $email, $phone);
 	
 	if (!isset($_POST['submitted'])) {
 		header('Location:register');
@@ -23,11 +22,11 @@
 		exit;
 	}
 	
-	require_once 'lib.php';
-	
+	$success = addPerson($name, $email, $phone);	
 	if ($success) {
 		header('Location:register-success');
 	} else {
 		header('Location:register?error_general');
+		exit;
 	}
 ?>
