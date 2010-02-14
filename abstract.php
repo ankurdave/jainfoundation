46f16545ce7d3183decdbeb069e1b5a55cfae096
Ankur Dave <ankurdave@gmail.com>
2010-02-14 14:18:50 -0800

    Undid multitext changes from 67:c290505c5bba, but kept the rest of the input 100% changes

--- abstract.php
+++ abstract.php
@@ -174,9 +174,9 @@
 		<tr>
 			<th></th>
 			<th>First Name (<span class="required_indicator">*</span>)</th>
-			<th>M.I.</th>
+			<th>Middle Initial</th>
 			<th>Last Name (<span class="required_indicator">*</span>)</th>
-			<th>Affil. (<span class="required_indicator">*</span>)</th>
+			<th>Affiliation (<span class="required_indicator">*</span>)</th>
 		</tr>
 		
 		<?php
@@ -190,9 +190,6 @@
 				array(
 					'label' => "Author #$i",
 					'class' => array(
-						'_firstname' => array('author_firstname'),
-						'_middlename' => array('author_middlename'),
-						'_lastname' => array('author_lastname'),
 						'_affiliation' => array('affiliation_reference'),
 					),
 					'value' => $values,
