415d2c472b3d2b33d57b4ce35ba2312320bb9668
Ankur Dave <ankurdave@gmail.com>
2010-03-15 21:51:31 -0700

    Made hotel reservation page changes

--- location.php
+++ location.php
@@ -8,7 +8,21 @@
 
 <?php include 'includes/menu.inc.php' ?>
 
-<p>Details coming soon.</p>
+<p>We are excited that you will be able to attend.  If you have any questions about the reservation process, please don't hesitate to contact Angela Salerno at the Jain Foundation (<a href="mailto:asalerno@jain-foundation.org">asalerno@jain-foundation.org</a> or 425-882-1492).</p>
+
+<p class="intext"><img src="images/hotel-reservation.jpg" /></p>
+
+<p>This year's conference is being held at the <a href="http://bellevue.hyatt.com/hyatt/hotels/index.jsp">Hyatt Regency Bellevue</a> in Bellevue, WA from September 11&ndash;14, 2010.  The Hyatt Regency Bellevue hotel is only 5&ndash;10 minutes from the Jain Foundation, Inc. office. If you would like to visit the office while you are in Seattle, please contact Angela Salerno at <a href="mailto:asalerno@jain-foundation.org">asalerno@jain-foundation.org</a>.</p>
+
+<p>A guaranteed block of rooms has been reserved for our conference for the nights of September 11, 12, and 13, 2010.  <strong>Reservations must be made no later than August 20th in order to be eligible for the special Jain Foundation conference rate.  The conference rate is $142/night + tax for single/double occupancy for a basic King or Double.</strong>   A third or fourth person in the room is $25 per night extra for each additional person.  Please feel free to upgrade your room at your own cost. </p>
+
+<p>In addition, the conference rate ($142/night + tax) is available for three days pre and post the conference (September 8&ndash;17).   However, these rooms are provided based on availability so book your room early.  </p>
+
+<p><strong>Please note:  Attendees who will be sharing a room with a roommate assigned by the Jain Foundation should not book their hotel reservations directly.  Please contact Angela Salerno (<a href="mailto:asalerno@jain-foundation.org">asalerno@jain-foundation.org</a>) at the Jain Foundation with your arrival and departure dates and she will make your reservations and send you a confirmation. </strong></p>
+
+<p>Booking a reservation online is simple. <a href="http://resweb.passkey.com/Resweb.do?mode=welcome_ei_new&eventID=2503720&fromResdesk=true">Begin the process here</a>. If you prefer to speak with someone directly you can make your reservation by calling the Passkey's Toll Free Number: 888-421-1442 and indicate that you are making a reservation for the Jain Foundation Dysferlin Conference. If you get a message saying that the conference rate is not available or if you have any other problems, please do not book your room.  Instead contact Angela Salerno (<a href="mailto:asalerno@jain-foundation.org">asalerno@jain-foundation.org</a>) at the Jain Foundation so that she can help you with your reservation. </p>
+
+<p>We look forward to seeing you in September and having an informative and productive conference!!!</p>
 
 <?php
 	printFooter();
