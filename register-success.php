04c59daf687b034efaadf98f1bb4baa186979a30
Ankur Dave <ankurdave@gmail.com>
2010-03-18 16:15:12 -0700

    Updated registration thank-you page according to Nilah's request

--- register-success.php
+++ register-success.php
@@ -8,7 +8,18 @@
 
 <?php include 'includes/menu.inc.php' ?>
 
-<p>Thank you for registering.</p>
+<p>Thank you for registering. Please print this page for your records.</p>
+
+<p>We will let you know if your registration has been approved within the next week.  If you do not hear from us within a week or have any questions please contact Angela Salerno at <a href="mailto:asalerno@jain-foundation.org">asalerno@jain-foundation.org</a>.</p>
+
+<p>Please send checks to the Jain Foundation at the following address:</p>
+
+<p>
+	Jain Foundation<br />
+	2310 130th Ave NE<br />
+	Suite B101<br />
+	Bellevue, WA 98005
+</p>
 
 <?php
 	printFooter();
