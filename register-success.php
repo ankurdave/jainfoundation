3e1d42dc37a1112d57854ea4029d22dd46601107
Ankur Dave <ankurdave@gmail.com>
2010-03-18 16:46:09 -0700

    Finished changes to registration thank-you page

--- register-success.php
+++ register-success.php
@@ -21,6 +21,20 @@
 	Bellevue, WA 98005
 </p>
 
+<p>Please send PayPal receipt to the Jain Foundation by one of the following methods:</p>
+
+<ul>
+	<li>Email to Angela Salerno at <a href="mailto:asalerno@jain-foundation.org">asalerno@jain-foundation.org</a></li>
+	<li>Fax to the Jain Foundation at 425-882-1050</li>
+	<li>
+		Mailed to the Jain Foundation at:
+		<br />Jain Foundation
+		<br />2310 130th Ave NE
+		<br />Suite B101
+		<br />Bellevue, WA 98005
+	</li>
+</ul>
+
 <?php
 	printFooter();
 ?>
