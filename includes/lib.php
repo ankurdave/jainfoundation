c84f3ec6857f3ff153c9dd5a01d87e1978e94371
Ankur Dave <ankurdave@gmail.com>
2010-03-23 18:16:04 -0700

    Finished up moving.
    Added forgotten files that were accidentally deleted in the previous commit.

---
+++ includes/lib.php
@@ -0,0 +1,12 @@
+<?php
+
+include('config.php');
+
+// Include the core libraries
+include('lib-database.php');
+include('lib-template.php');
+include('lib-forms.php');
+include('lib-msexcel.php');
+include('lib-util.php');
+
+?>
