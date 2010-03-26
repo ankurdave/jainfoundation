4004948d830fb5fde0e2e1e2af6f1f6c1b9c7fa0
Ankur Dave <ankurdave@gmail.com>
2010-03-26 16:04:15 -0700

    Started converting database libraries to object-oriented.
    Created Abstract DAO and InsertUpdateQuery object.

--- includes/lib-database.php
+++ includes/lib-database.php
@@ -5,6 +5,10 @@ function connectToDB() {
 	
 	$db = new mysqli($Config['DB']['Host'], $Config['DB']['User'], $Config['DB']['Password'], $Config['DB']['Database']);
 	
+	if (mysqli_connect_error()) {
+		throw new Exception("Error connecting to database: " . mysqli_connect_error);
+	}
+	
 	// Make sure Unicode works
 	$db->query("SET NAMES 'utf8' COLLATE 'utf8_unicode_ci'");
 	
@@ -15,5 +19,102 @@ function connectToDB() {
 function make_column_update_sql($col) {
 	return "$col=VALUES($col)";
 };
+
+/**
+ * Represents a MySQLi database query that can insert a row or update an existing row.
+ */
+class InsertUpdateQuery {
+	private $db;
+	private $table;
+	private $columns;
+	
+	function __construct($db) {
+		$this->db = $db;
+	}
+	
+	function setTable($table) {
+		$this->table = $table;
+	}
+	
+	function setColumn($name, $val, $type) {
+		$col = new QueryColumn();
+		$col->name = $name;
+		$col->value = $val;
+		$col->type = $type;
+		
+		$this->columns[$name] = $col;
+	}
+	
+	function execute() {
+		// Build the query
+		$query = $this->db->prepare($this->getQuerySQL());
+		if (!$query) {
+			throw new Exception("Error preparing query: " . $this->getQuerySQL());
+		}
+		
+		// Bind the parameters
+		$paramTypes = '';
+		$colVals = array();
+		foreach ($this->columns as $colName => $col) {
+			$paramTypes .= ($col->type == 'file') ? 'b' : $col->type;
+			$colVals[] = $col->value;
+		}
+		
+		call_user_func_array(array(&$query, 'bind_param'), array_merge(array($paramTypes), $colVals));
+		
+		// Send the long column data
+		$colNum = 0;
+		foreach ($this->columns as $colName => $col) {
+			if ($col->type == 'file') {
+				$this->bindFileToQuery($colNum, $col->value, $query);
+			}
+			
+			$colNum++;
+		}
+		
+		// Run the query
+		$query->execute();
+		if ($this->db->error) {
+			throw new Exception("Error executing query: $this->db->error");
+		}
+		
+		$query->close();
+	}
+	
+	private function bindFileToQuery($colNum, $filename, $query) {
+		$filehandle = @fopen($filename, 'r');
+		if ($filehandle) {
+			while (!feof($filehandle)) {
+				$query->send_long_data($colNum, fread($filehandle, 8192));
+			}
+			fclose($filehandle);
+		}
+	}
+	
+	private function getQuerySQL() {
+		return "INSERT INTO $this->table (" . $this->getColNamesSQL() . ") VALUES (" . $this->getColPlaceholdersSQL() . ") ON DUPLICATE KEY UPDATE " . $this->getColUpdateSQL();
+	}
+
+	private function getColNamesSQL() {
+		return join(', ', array_keys($this->columns));
+	}
+	
+	private function getColPlaceholdersSQL() {
+		return join(', ', array_fill(0, count($this->columns), '?'));
+	}
+	
+	private function getColUpdateSQL() {
+		return join(', ', array_map('make_column_update_sql', array_keys($this->columns)));
+	}
+}
+
+/**
+ * Represents a column in a SQL query.
+ */
+class QueryColumn {
+	public $name;
+	public $value;
+	public $type;
+}
 
 ?>
