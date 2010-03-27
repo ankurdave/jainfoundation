076bd3877f2e9713b5e48de2b5ed02270c86e496
Ankur Dave <ankurdave@gmail.com>
2010-03-26 20:29:35 -0700

    Updated abstract-show.php to use AbstractDAO

--- conference2010/abstract-show.php
+++ conference2010/abstract-show.php
@@ -3,25 +3,24 @@
 	
 	$form_location = 'abstract.php';
 	
+	// Load the abstract using the DAO
 	if (isset($_GET['id'])) {
-		$data = getAbstract($_GET['id']);
+		$abstract = new AbstractDAO($_GET['id']);
 	} else if (isset($_COOKIE['id'])) {
-		$data = getAbstract($_COOKIE['id']);
-	}
-
-	if (!$data) {
+		$abstract = new AbstractDAO($_COOKIE['id']);
+	} else {
 		header("Location: $form_location");
 		exit;
 	}
 	
 	// Escape all data fields before printing
-	$data_raw = $data;
-	$data = array_map('print_html', $data);
+	$data = array_map('print_html', $abstract->getFields());
+	$data_raw = $abstract->getFields();
 	
 	// Output a Word document with HTML in it
 	// See http://stackoverflow.com/questions/124959/create-word-document-using-php-in-linux#answer-125009
 	header("Content-Type: application/vnd.ms-word");
-	header("Content-Disposition: attachment; filename=abstract-" . $data['id'] . ".doc");
+	header("Content-Disposition: attachment; filename=abstract-" . urlencode($data['id']) . ".doc");
 	
 	include 'includes/abstract-show-template-head.inc.php';
 	include 'includes/abstract-show-template-body.inc.php';
