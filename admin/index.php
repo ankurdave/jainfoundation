158859895425fec6dcd83de9a29820acfa57ec5a
Ankur Dave <ankurdave@gmail.com>
2010-03-31 15:36:06 -0700

    Organized admin index page, added abstract list

--- admin/index.php
+++ admin/index.php
@@ -6,11 +6,23 @@
 
 <h1>Admin Pages</h1>
 
+<h2>Patient registrations</h2>
 <ul>
-	<li><a href="<?php echo $Config['URLPath'] ?>/conference2010/register-export.php">Export registrations</a></li>
-	<li><a href="<?php echo $Config['URLPath'] ?>/conference2010/register-list.php">List registrations</a></li>
-	<li><a href="<?php echo $Config['URLPath'] ?>/conference2010/abstract-show-all.php">Export abstracts</a></li>
-	<li><a href="<?php echo $Config['URLPath'] ?>/conference2010/abstract-export.php">Export abstract submission info</a></li>
+	<li><a href="data.php">List patient registrations</a></li>
+	<li><a href="export.php">Export patient registrations</a> (Excel spreadsheet)</li>
+</ul>
+
+<h2>Conference 2010 registrations</h2>
+<ul>
+	<li><a href="<?php echo $Config['URLPath'] ?>/conference2010/register-list.php">List conference registrations</a></li>
+	<li><a href="<?php echo $Config['URLPath'] ?>/conference2010/register-export.php">Export conference registrations</a> (Excel spreadsheet)</li>
+</ul>
+
+<h2>Conference 2010 abstracts</h2>
+<ul>
+	<li><a href="<?php echo $Config['URLPath'] ?>/conference2010/abstract-list.php">List abstract submissions</a></li>
+	<li><a href="<?php echo $Config['URLPath'] ?>/conference2010/abstract-export.php">Export abstract submissions</a> (Excel spreadsheet)</li>
+	<li><a href="<?php echo $Config['URLPath'] ?>/conference2010/abstract-show-all.php">Export abstracts</a> (Word document)</li>
 </ul>
 
 <?php
---
+++ conference2010/abstract-list.php
@@ -0,0 +1,46 @@
+<?php
+	require 'includes/lib.php';
+
+	printHeader(array('title' => 'Conference 2010 | List of Abstracts'));
+
+	$db = connectToDB();
+	$result = $db->query('SELECT id, firstname, middlename, lastname, degree, department, institution, street_address, city, state_province, zip_postal_code, country, phone, fax, email, author_status, degree_year, abstract_category, abstract_category_other, presentation_type, abstract_title, comments FROM abstract');
+?>
+
+<h1>Registrants</h1>
+
+<?php include 'includes/menu.inc.php' ?>
+
+<?php
+	$i = 0;
+	while ($data = $result->fetch_assoc()) {
+		$i++;
+?>
+		<table class="db_list_entry" id="abstract<?php print_html($data['id']) ?>">
+<?php
+		$i = 0;
+		foreach ($data as $key => $val) { ?>
+			<tr class="<?php echo $i % 2 == 0 ? 'row_even' : 'row_odd' ?>">
+				<th><?php echo print_html($key) ?></th>
+				<td><?php echo print_html($val) ?></td>
+				<td class="action">
+					<?php
+						if ($key == 'id') {
+							?>
+							(<a href="abstract-delete.php?id=<?php echo urlencode($data['id']) ?>">delete</a>)
+							<?php
+						}
+					?>
+				</td>
+			</tr>
+<?php
+			$i++;
+		} ?>
+		</table>
+<?php
+	}
+
+	$result->free();
+
+	printFooter();
+?>
