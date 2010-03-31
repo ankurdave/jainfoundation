853ff4c7b233d31e143f86f859f7fe23b2ef740b
Ankur Dave <ankurdave@gmail.com>
2010-03-31 15:54:33 -0700

    Updated admin/export.php with the new changes
    --HG--
    rename : admin/export-new.php => admin/export.php

--- admin/export.php
+++ admin/export.php
@@ -1,10 +1,56 @@
 <?	session_start();	if(isset($_SESSION['SESSION_ID'])){		$login = true;	}else{		session_destroy();		$login=false;		include "login.php";		exit;	}?><?include('includes/mysql.php');
 
+// See http://php.net/manual/en/function.html-entity-decode.php
+// This is necessary because html_entity_decode isn't working properly on this server (PHP 4.3.9) for some reason
+function unhtmlentities($string) {
+	// replace numeric entities
+	$string = preg_replace('~&#x([0-9a-f]+);~ei', 'unichr(hexdec("\\1"))', $string);
+	$string = preg_replace('~&#([0-9]+);~e', 'unichr("\\1")', $string);
+	// replace literal entities
+	$trans_tbl = get_html_translation_table(HTML_ENTITIES);
+	$trans_tbl = array_flip($trans_tbl);
+	return strtr($string, $trans_tbl);
+}
+function unichr($u) {
+	return mb_convert_encoding('&#' . intval($u) . ';', 'UTF-8', 'HTML-ENTITIES');
+}
+// XLS creation tools
+function xlsBegin() {
+    return chr(255) . chr(254);
+}
+function xlsWriteRow($row) {
+    return convert_utf8_to_utf16(join("\t", $row) . "\r\n");
+}
+// Returns UTF-16 text
+// See http://xhtml.net/breves/435-Un-parlement-ouvert-et-au-passage-Excel-csv-et-utf-8
+function convert_utf8_to_utf16($text) {
+	return mb_convert_encoding($text, 'UTF-16LE', 'UTF-8');
+}
+function convert_mixed_to_utf8_no_linebreaks($text) {
+	$text = str_replace("\n", '', $text);
+	$text = str_replace("\r", '', $text);
+	$text = utf8_encode($text);
+	$text = unhtmlentities($text);
+	return $text;
+}
 $select = "SELECT * FROM registrants order by patient_id desc";$export = mysql_query($select);$fields = mysql_num_fields($export); 
 
 // Print the HTTP headers
 header("Content-Type: text/csv");header("Content-Disposition: attachment; filename=patient-data.csv");
 
-while($row = mysql_fetch_row($export)) {    $line = '';    foreach($row as $value) {                                                    if ((!isset($value)) OR ($value == "")) {            $value = "\t";        } else {
-            $value = str_replace('"', '""', $value);            $value = '"' . $value . '"' . "\t";        }        $line .= $value;    }    $data .= trim($line)."\n";}$data = str_replace("\r","",$data); if ($data == "") {    $data = "\n(0) Records Found!\n";                        } $csv = "$header\n$data"; //base64_decode(#HTTP_POST_VARS["csv_data"]) ;header("Expires: Mon, 26 Jul 1997 05:00:00 GMT") ;header("Pragma: no-cahce") ;header("Content-Type: application/vnd.ms-excel");header("Content-Disposition: attachment; filename=".uniqid('').'.xls');header("Expires: 0");header("Cache-Control: must-revalidate, post-check=0,pre-check=0");header("Content-Type: application/vnd.ms-excel");header("Content-Disposition: \"inline\"");header("Content-Length: " . strlen($csv));echo $csv ;//@readfile("$header\n$data");?>
-?>
+// IE has a bug that breaks downloads from SSL sites with the no-cache header set (see http://support.microsoft.com/kb/812935).
+// The previous developer had used the following header:
+// header("Pragma: no-cahce"); // note the typo!
+// The typo caused the Pragma header to be reset, avoiding the bug. If the typo is fixed, the bug is triggered. If the entire line is removed, PHP still defaults to no-cache, again triggering the bug (see http://www.php.net/manual/en/function.header.php#46846).
+// Therefore we must clear the caching header.
+header('Pragma:');
+
+// Print the data
+print xlsBegin();
+
+$header = array();
+for ($i = 0; $i < $fields; $i++) {    $header[] = mysql_field_name($export, $i);}
+print xlsWriteRow($header);
+while($row = mysql_fetch_row($export)) {
+	print xlsWriteRow(array_map('convert_mixed_to_utf8_no_linebreaks', $row));
+}?>
