e6d0510ef78e7f0758c90013814027fb8da7269a
Ankur Dave <ankurdave@gmail.com>
2010-04-01 13:07:02 -0700

    Centered "Registration" on the conference button

--- conference2010/includes/menu.inc.php
+++ conference2010/includes/menu.inc.php
@@ -1,7 +1,7 @@
 <div id="page_nav_wrapper">
 	<ul id="page_nav">
 		<li id="information"><a href="information.php">General <br />Information</a></li>
-		<li id="register"><a href="register.php">Registration</a></li>
+		<li id="register"><a href="register.php" style="line-height:3em">Registration</a></li>
 		<li id="abstract"><a href="abstract.php">Abstract <br />Submission</a></li>
 		<li id="location"><a href="location.php">Hotel <br />Reservations</a></li>
 		<li id="program"><a href="program.php">Scientific <br />Program</a></li>
