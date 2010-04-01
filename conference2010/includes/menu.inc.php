9d496fd4a20956ddf7a811b1d5bd962ac8e7268e
Ankur Dave <ankurdave@gmail.com>
2010-04-01 13:05:55 -0700

    Added linebreaks to conference menu

--- conference2010/includes/menu.inc.php
+++ conference2010/includes/menu.inc.php
@@ -1,10 +1,10 @@
 <div id="page_nav_wrapper">
 	<ul id="page_nav">
-		<li id="information"><a href="information.php">General Information</a></li>
+		<li id="information"><a href="information.php">General <br />Information</a></li>
 		<li id="register"><a href="register.php">Registration</a></li>
-		<li id="abstract"><a href="abstract.php">Abstract Submission</a></li>
-		<li id="location"><a href="location.php">Hotel Reservations</a></li>
-		<li id="program"><a href="program.php">Scientific Program</a></li>
+		<li id="abstract"><a href="abstract.php">Abstract <br />Submission</a></li>
+		<li id="location"><a href="location.php">Hotel <br />Reservations</a></li>
+		<li id="program"><a href="program.php">Scientific <br />Program</a></li>
 	</ul>
 </div>
 <div style="clear:both"></div>
