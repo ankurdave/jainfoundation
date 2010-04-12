21d17217112f82cccb34f52ed09e971002b55708
Ankur Dave <ankurdave@gmail.com>
2010-04-12 14:43:36 -0400

    Added addable and removable error highlighting using highlight/unhighlight methods

--- conference2010/js/register.js
+++ conference2010/js/register.js
@@ -81,6 +81,7 @@ $(document).ready(function() {
 				requiredIfFieldChecked: "#meals_gala_dinner input[type=radio][value='yes']"
 			}
 		},
+
 		errorPlacement: function(error, element) {
 			// If we're in a table, but not a multi-text one, then put the error in the next cell. Otherwise do the default action.
 			if (element.closest("table") && !element.closest("table").hasClass("multitext")) {
@@ -88,10 +89,22 @@ $(document).ready(function() {
 			} else {
 				error.insertAfter(element);
 			}
+		},
 
-			// If the element is a radio button, also add a "class='error'" to div#ELEMENT_NAME
-			if (element.attr("type") == "radio") {
-				$("div#" + element.attr("name")).addClass("error");
+		highlight: function(element, errorClass, validClass) {
+			$.validator.defaults.highlight.call(this, element, errorClass, validClass);
+			
+			// If the element is a radio button, also add and remove the appropriate classes from div#ELEMENT_NAME
+			if ($(element).attr("type") == "radio") {
+				$.validator.defaults.highlight.call(this, "div#" + $(element).attr("name"), errorClass, validClass);
+			}
+		},
+		unhighlight: function(element, errorClass, validClass) {
+			$.validator.defaults.unhighlight.call(this, element, errorClass, validClass);
+			
+			// If the element is a radio button, also add and remove the appropriate classes from div#ELEMENT_NAME
+			if ($(element).attr("type") == "radio") {
+				$.validator.defaults.unhighlight.call(this, "div#" + $(element).attr("name"), errorClass, validClass);
 			}
 		}
 	});
