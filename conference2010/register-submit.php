1cd88491e51f91ef82b37346f5411766023e5ec3
Ankur Dave <ankurdave@gmail.com>
2010-04-12 21:15:22 -0400

    Updated register-submit.php to use RegistrantGalaGuestDAO

--- conference2010/register-submit.php
+++ conference2010/register-submit.php
@@ -23,6 +23,33 @@
 		$registrant->setField($field, $val);
 	}
 
+	// Send the gala guest info to the DAO
+	$galaGuests = array();
+	foreach ($_POST as $field => $val) {
+		if (empty($val)) {
+			continue;
+		}
+
+		if (preg_match('/^meals_gala_dinner_guest_(\d+)_(.*)$/i', $field, $matches)) {
+			if ($galaGuests[$matches[1]] === null) {
+				$galaGuests[$matches[1]] = new RegistrantGalaGuestDAO($db);
+			}
+
+			$galaGuests[$matches[1]]->setField($matches[2], $val);
+		}
+	}
+	$registrant->clearGalaGuests();
+	foreach ($galaGuests as $galaGuest) {
+		$registrant->addGalaGuest($galaGuest);
+	}
+
+	// Send the uploaded picture to the DAO
+	// Important: This MUST come after the $_POST import, otherwise it would be possible to set a POST field called 'picture_data' to an arbitrary file system path
+	if (is_uploaded_file($_FILES['picture']['tmp_name']) && $_FILES['picture']['size'] <= 1000000) {
+		$abstract->setField('picture_data', $_FILES['picture']['tmp_name']);
+		$abstract->setField('picture_mimetype', $_FILES['picture']['type']);
+	}
+
 	// Save the DAO
 	// This is before validation so that if there's an error, the user won't lose the data
 	try {
