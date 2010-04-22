7e9881490fb7e285c0c946215db21e437ab542eb
Ankur Dave <ankurdave@gmail.com>
2010-04-21 19:22:40 -0700

    Combined menu items for Registration and Abstract Submission into one

--- conference2010/includes/menu.inc.php
+++ conference2010/includes/menu.inc.php
@@ -1,10 +1,9 @@
 <div id="page_nav_wrapper">
 	<ul id="page_nav">
-		<li id="information"><a href="information.php">General <br />Information</a></li>
-		<li id="register"><a href="register.php">Meeting <br />Registration</a></li>
-		<li id="abstract"><a href="abstract.php">Abstract <br />Submission</a></li>
-		<li id="location"><a href="location.php">Hotel <br />Reservations</a></li>
-		<li id="program"><a href="program.php">Scientific <br />Program</a></li>
+		<li id="information" class="twoninths"><a href="information.php">General <br />Information</a></li>
+		<li id="register" class="onethird"><a href="register.php">Meeting Registration <br/>and Abstract Submission</a></li>
+		<li id="location" class="twoninths"><a href="location.php">Hotel <br />Reservations</a></li>
+		<li id="program" class="twoninths"><a href="program.php">Scientific <br />Program</a></li>
 	</ul>
 </div>
 <div style="clear:both"></div>
