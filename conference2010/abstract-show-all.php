95d314a5a21145d2304d2e55b37e7a194ecf764d
Ankur Dave <ankurdave@gmail.com>
2010-04-26 22:46:39 -0700

    Fixed function call typo

--- conference2010/abstract-show-all.php
+++ conference2010/abstract-show-all.php
@@ -8,7 +8,7 @@ header("Content-Disposition: attachment; filename=abstracts.doc");
 printAbstractHead();
 
 $db = connectToDB();
-$abstracts = AbstractDAO::loadAll();
+$abstracts = AbstractDAO::loadAll($db);
 foreach ($abstracts as $abstract) {
 	printAbstractBody($abstract);
 }
