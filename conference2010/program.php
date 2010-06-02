3aa4723b71cf7c8843fcac8a9c27d0ed93184b17
Ankur Dave <ankurdave@gmail.com>
2010-06-01 19:21:04 -0700

    Made Angela changes to Scientific Program

--- conference2010/program.php
+++ conference2010/program.php
@@ -14,17 +14,71 @@
 
 <p>Below is the tentative meeting schedule for the Fourth Annual Dysferlin Conference.   This page will be updated as soon as more details are available.</p>
 
-<h2 class="day">Saturday, September 11th, 2010</h2>
-<h3 class="time">2 pm – 6 pm</h3>
+<h3>Session Topics</h3>
 
-<h2 class="day">Sunday, September 12th, 2010</h2>
-<h3 class="time">8 am – 5 pm</h3>
+<p class="note">Talks will be assigned to one or more of the following sessions.</p>
 
-<h2 class="day">Monday, September 13th, 2010</h2>
-<h3 class="time">8 am – 5 pm</h3>
+<table>
+<tr><th>Session I:</th> <td>What does dysferlin look like?</td></tr>
+<tr><th>Session II:</th> 	 <td>Where is dysferlin localized?</td></tr>
+<tr><th>Session III:</th> 	 <td>How does dysferlin traffic?</td></tr>
+<tr><th>Session IV:</th> 	 <td>What does dysferlin do?</td></tr>
+<tr><th>Session V:</th> 	 <td>What is the role of inflammation in dysferlinopathy?</td></tr>
+<tr><th>Session VI:</th> 	 <td>What do dysferlin’s partners do?</td></tr>
+<tr><th>Session VII:</th>	 <td>Is there a regeneration defect in dysferlinopathy?</td></tr>
+<tr><th>Session VIII:</th>   <td>How do we compensate for dysferlin absence?</td></tr>
+<tr><th>Session IX:</th>     <td>How can we put dysferlin back?</td></tr>
+<tr><th>Session X:</th>      <td>What tools are available for dysferlin research?</td></tr>
+</table>
 
-<h2 class="day">Tuesday, September 14th, 2010</h2>
-<h3 class="time">8 am – 12 pm</h3>
+<h3>Committed Attendees</h3>
+
+<ul>
+<li>Eric Hoffman</li>
+<li>Jeff Chamberlain</li>
+<li>Jeff Molkentin</li>
+<li>Terry Partridge</li>
+<li>Nicolas Lévy</li>
+<li>Silvere van der Maarel</li>
+<li>Robert Bloch</li>
+<li>Miranda Grounds</li>
+<li>Zipora Yablonka-Reuveni</li>
+<li>Gillian Butler-Browne</li>
+<li>Vincent Mouly</li>
+<li>Isabelle Richard</li>
+<li>Sandra Cooper</li>
+<li>Steven Vogel</li>
+<li>Michele Calos</li>
+<li>Jianje Ma</li>
+<li>Barbara Wakimoto</li>
+<li>Eduard Gallardo</li>
+<li>Annamaria De Luca</li>
+<li>Rumaisa Bashir</li>
+<li>Bryan Sutton</li>
+<li>Daniel Stockholm</li>
+<li>Noah Weisleder</li>
+<li>Martin Krahn</li>
+<li>Steve Laval</li>
+<li>Joseph Roche</li>
+<li>Jordi Diaz-Manera</li>
+<li>Antoine de Morrée</li>
+<li>Maura Parker</li>
+<li>Marc Bartoli</li>
+</ul>
+
+<h3>Tentative Meeting Schedule</h3>
+
+<h4 class="day">Saturday, September 11th, 2010</h4>
+<h5 class="time">2 pm – 6 pm</h5>
+
+<h4 class="day">Sunday, September 12th, 2010</h4>
+<h5 class="time">8 am – 5 pm</h5>
+
+<h4 class="day">Monday, September 13th, 2010</h4>
+<h5 class="time">8 am – 5 pm</h5>
+
+<h4 class="day">Tuesday, September 14th, 2010</h4>
+<h5 class="time">8 am – 12 pm</h5>
 
 
 <?php
