6d03bd52bfb7b6378ab04a7056f6849cad652739
Ankur Dave <ankurdave@gmail.com>
2010-06-09 14:34:29 -0700

    Updated text of Scientific Program page

--- conference2010/program.php
+++ conference2010/program.php
@@ -23,59 +23,74 @@
 <tr><th>Session II:</th> 	 <td>Where is dysferlin localized?</td></tr>
 <tr><th>Session III:</th> 	 <td>How does dysferlin traffic?</td></tr>
 <tr><th>Session IV:</th> 	 <td>What does dysferlin do?</td></tr>
-<tr><th>Session V:</th> 	 <td>What is the role of inflammation in dysferlinopathy?</td></tr>
-<tr><th>Session VI:</th> 	 <td>What do dysferlin’s partners do?</td></tr>
-<tr><th>Session VII:</th>	 <td>Is there a regeneration defect in dysferlinopathy?</td></tr>
-<tr><th>Session VIII:</th>   <td>How do we compensate for dysferlin absence?</td></tr>
-<tr><th>Session IX:</th>     <td>How can we put dysferlin back?</td></tr>
-<tr><th>Session X:</th>      <td>What tools are available for dysferlin research?</td></tr>
+<tr><th>Session V:</th> 	 <td>What do dysferlin’s partners do?</td></tr>
+<tr><th>Session VI:</th> 	 <td>What does dysferlin-deficient muscle look like?</td></tr>
+<tr><th>Session VII:</th>	 <td>What is the role of inflammation in dysferlinopathy?</td></tr>
+<tr><th>Session VIII:</th>   <td>Is there a regeneration defect in dysferlinopathy?</td></tr>
+<tr><th>Session IX:</th>     <td>How do we compensate for dysferlin’s absence?</td></tr>
+<tr><th>Session X:</th>      <td>How can we put dysferlin back?</td></tr>
+<tr><th>Session XI:</th>     <td>What tools are available for dysferlin research?</td></tr>
 </table>
 
-<h3>Committed Attendees</h3>
+<h3>Confirmed Attendees</h3>
 
-<ul>
-<li>Eric Hoffman</li>
-<li>Robert Brown</li>
-<li>Lee Sweeney</li>
-<li>Jeff Chamberlain</li>
-<li>Jeff Molkentin</li>
-<li>Terry Partridge</li>
-<li>Nicolas Lévy</li>
-<li>Silvere van der Maarel</li>
-<li>Robert Bloch</li>
-<li>Miranda Grounds</li>
-<li>Zipora Yablonka-Reuveni</li>
-<li>Gillian Butler-Browne</li>
-<li>Vincent Mouly</li>
-<li>Isabelle Richard</li>
-<li>Sandra Cooper</li>
-<li>Steven Vogel</li>
-<li>Michele Calos</li>
-<li>Jianje Ma</li>
-<li>Barbara Wakimoto</li>
-<li>Eduard Gallardo</li>
-<li>Annamaria De Luca</li>
-<li>Rumaisa Bashir</li>
-<li>Bryan Sutton</li>
-<li>Daniel Stockholm</li>
-<li>Noah Weisleder</li>
-<li>Martin Krahn</li>
-<li>Renzhi Han</li>
-<li>Steve Laval</li>
-<li>Joseph Roche</li>
-<li>Jordi Diaz-Manera</li>
-<li>Antoine de Morrée</li>
-<li>Maura Parker</li>
-<li>Marc Bartoli</li>
-<li>Peter Pytel</li>
-<li>François Monjaret</li>
-<li>Perrine Borel</li>
-<li>Bradley Hodges</li>
-<li>Tatiana Cohen</li>
-<li>Alexis Demonbreun</li>
-<li>Rinse Klooster</li>
-<li>Peter Sazani</li>
-</ul>
+<table>
+<tr><th>Confirmed Attendee</th> <th>Institution</th></tr>
+<tr><td>Florian Barthelemy</td> <td>Universite de la Mediterranee-Marseille cedex 05, France</td></tr>
+<tr><td>Marc Bartoli </td> <td>Universite de la Mediterranee-Marseille cedex 05, France</td></tr>
+<tr><td>Rumaisa Bashir</td> <td>University of Durham-Durham, UK</td></tr>
+<tr><td>Pascal Bernatchez</td> <td>University of British Columbia/St. Paul's Hospital-Vancouver, B.C.  Canada</td></tr>
+<tr><td>Bob Bloch</td> <td>University of Maryland School of Medicine-Baltimore, MD</td></tr>
+<tr><td>Perrine Borel</td> <td>Genethon- Evry, France</td></tr>
+<tr><td>Bob Brown </td> <td>U MA Medical School-Worcester, MA </td></tr>
+<tr><td>Gillian Butler-Browne </td> <td>Institut de Myologie, Paris, France</td></tr>
+<tr><td>Michele Calos </td> <td>Stanford University School of Medicine-Stanford, CA </td></tr>
+<tr><td>Jeff Chamberlain</td> <td>University of Washington, Seattle, WA</td></tr>
+<tr><td>Tatiana Cohen </td> <td>Children's National Medical Center- Washington, D.C.</td></tr>
+<tr><td>Sandra Cooper</td> <td>The Children's Hospital at Westmead- Sydney, New South Wales, Australia</td></tr>
+<tr><td>Annamaria De Luca</td> <td>University of Bari-Bari, Italy</td></tr>
+<tr><td>Antoine de Morrée </td> <td>Leiden University Medical Center-Leiden, Netherlands</td></tr>
+<tr><td>Alexis Demonbreun </td> <td>University of Chicago-Chicago, IL</td></tr>
+<tr><td>Jordi Diaz-Manera</td> <td>Neurology Department. Hospital de la Santa Creu i Sant Pau-Barcelona, Spain</td></tr>
+<tr><td>Eduard Gallardo</td> <td>Neurology Department. Hospital de la Santa Creu i Sant Pau-Barcelona, Spain</td></tr>
+<tr><td>Miranda Grounds </td> <td>University of Western Australia-Crawley, WA, Australia</td></tr>
+<tr><td>Renzhi Han</td> <td>Loyola Univeristy Medical Center-Maywood, IL</td></tr>
+<tr><td>Matt Hirsch</td> <td>University of North Carolina-Chapel Hill, NC</td></tr>
+<tr><td>Bradley Hodges </td> <td>Prothelia Inc.- Milford, MA </td></tr>
+<tr><td>Eric Hoffman </td> <td>Children's National Medical Center- Washington, D.C.</td></tr>
+<tr><td>Akshay Jain</td> <td>Jain Foundation, Inc.-Seattle, WA</td></tr>
+<tr><td>Rinse Klooster </td> <td>Leiden University Medical Center-Leiden, Netherlands</td></tr>
+<tr><td>Martin Krahn</td> <td>Université de la Méditerranée-Marseille, France</td></tr>
+<tr><td>Steve Laval</td> <td>Newcastle University-Newcastle-upon-Tyne, Tyne and Wear  UK</td></tr>
+<tr><td>Angela Lek</td> <td>Institute for Neuroscience and Muscle Research- Westmead, NSW Australia</td></tr>
+<tr><td>Mokel Lek</td> <td>Institute for Neuroscience and Muscle Research- Westmead, NSW Australia</td></tr>
+<tr><td>Cleo Leung</td> <td>UBC James Hogg Research Centre, Providence Heart+Lung Institute- Vancouver, B.C.  Canada</td></tr>
+<tr><td>Nicolas Lévy </td> <td>INSERM-Marseille, France</td></tr>
+<tr><td>William Lostal</td> <td>GENETHON-Evry, France</td></tr>
+<tr><td>Jianje Ma </td> <td>TRIM-edicine- North Brunswick, NJ</td></tr>
+<tr><td>Elizabeth McNally</td> <td>University of Chicago- Chicago, IL</td></tr>
+<tr><td>Jeff Molkentin </td> <td>Cincinnati Children's Hospital Medical Center- Cincinnati, OH</td></tr>
+<tr><td>François Monjaret </td> <td>Genethon- Evry, France</td></tr>
+<tr><td>Vincent Mouly </td> <td>Institut de Myologie- Paris, France</td></tr>
+<tr><td>Maura Parker</td> <td>Fred Hutchinson Cancer Research Center-Seattle, WA</td></tr>
+<tr><td>Terry Partridge</td> <td>Children’s National Medical Center-Washington, D.C.</td></tr>
+<tr><td>Peter Pytel </td> <td>University of Chicago-Chicago, IL</td></tr>
+<tr><td>Wendy Resneck</td> <td>University of Maryland-Baltimore, MD</td></tr>
+<tr><td>Isabelle Richard </td> <td>Genethon- Evry, France</td></tr>
+<tr><td>Joseph Roche </td> <td>University of Maryland-Baltimore, MD</td></tr>
+<tr><td>Peter Sazani </td> <td>AVI BioPharma-Bothell, WA </td></tr>
+<tr><td>Kevin Sonnemann</td> <td>University of Wisconsin-Madison, WI</td></tr>
+<tr><td>Simone Spuler</td> <td>Charité Medical Faculty-Berlin, Deutschland</td></tr>
+<tr><td>Daniel Stockholm</td> <td>Genethon- Evry, France</td></tr>
+<tr><td>Bryan Sutton</td> <td>Texas Tech University Health Sciences Center-Lubbock, TX</td></tr>
+<tr><td>Lee Sweeney </td> <td>Univ. of Penn. School of Medicine, Philadelphia, PA </td></tr>
+<tr><td>Silvere van der Maarel</td> <td>Leiden University Medical Center-Leiden, Netherlands</td></tr>
+<tr><td>Steven Vogel </td> <td>NIAAA/NIH-Rockville, MD</td></tr>
+<tr><td>Barbara Wakimoto</td> <td>Univeristy of Washington- Seattle, WA </td></tr>
+<tr><td>Noah Weisleder</td> <td>TRIM-edicine- North Brunswick, NJ</td></tr>
+<tr><td>Zipora Yablonka-Reuveni</td> <td>University of Washington School of Medicine-Seattle, WA</td></tr>
+<tr><td>Joshua Zimmerberg</td> <td>NICHD, NIH-Bethesda, MD</td></tr>
+</table>
 
 <h3>Tentative Meeting Schedule</h3>
 
